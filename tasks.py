0f47335eae06f0a4b9bbf22174f850160eb34bef
xamidovscode <xamidovcoder@gmail.com>
2026-05-11 10:41:33 +0500

    jwt updated

--- tasks.py
+++ tasks.py
@@ -1,10 +1,17 @@
 from invoke import task
 
+
 @task
 def build(c):
     c.run("docker compose up --build -d")
 
+
 @task
 def down(c):
     c.run("docker compose down")
+
+
+@task
+def migrate(c):
+    c.run("docker compose exec hr-seller-api alembic upgrade head")
 
