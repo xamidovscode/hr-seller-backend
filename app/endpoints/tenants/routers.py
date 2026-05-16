a5348794411fe81df8753ce092fadf0061848a22
xamidovscode <xamidovcoder@gmail.com>
2026-05-16 17:40:35 +0500

    crud

--- app/endpoints/tenants/routers.py
+++ app/endpoints/tenants/routers.py
@@ -7,22 +7,34 @@ from app.resources.permissions.dependencies import require_roles
 
 router = APIRouter(prefix='/tenants', tags=["tenants"])
 
-@router.get('/all/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
+@router.get(
+    '/all/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))]
+)
 async def get_all_tenants(service: tenant_service):
     return await service.get_all_tenants()
 
 
-@router.post('/create/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
+@router.post(
+    '/create/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))]
+)
 async def create_tenant(service: tenant_service, schema: TenantCreateSchema):
     return await service.create_tenant(schema)
 
 
-@router.get('/{tenant_id}/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
+@router.get(
+    '/{tenant_id}/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))]
+)
 async def get_tenant_detail(tenant_id: int, service: tenant_service):
     return await service.tenant_detail(tenant_id=tenant_id)
 
 
-@router.patch('/{tenant_id}/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
+@router.patch(
+    '/{tenant_id}/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))]
+)
 async def update_tenant(tenant_id: int, service: tenant_service, schema: TenantUpdateSchema):
     return await service.tenant_update(tenant_id=tenant_id, schema=schema)
 
