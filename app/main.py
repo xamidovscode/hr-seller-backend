7e9a40831c93b30195b07b60ca332dc0d5fefa2c
xamidovscode <xamidovcoder@gmail.com>
2026-05-18 15:34:51 +0500

    sql admin removed

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from sqladmin import Admin
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.core.settings import settings
 from app.endpoints.base_route import base_v1_router
-from app.core.db import engine
-from app.models.admin import all_views
-from app.resources.permissions.admin_auth import AdminAuth
 
 app = FastAPI(
     title='HR SELLER APP',
@@ -27,10 +23,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
-
-admin = Admin(app, engine, authentication_backend=AdminAuth(secret_key=settings.SECRET_KEY))
-for view in all_views:
-    admin.add_view(view)
 
 app.include_router(base_v1_router, prefix="/api/v1")
 
