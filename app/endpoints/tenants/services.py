44e80cf6191a1dfc805947ead1a909b4abe903e5
xamidovscode <xamidovcoder@gmail.com>
2026-05-18 11:22:27 +0500

    crud

--- app/endpoints/tenants/services.py
+++ app/endpoints/tenants/services.py
@@ -127,7 +127,7 @@ class MonthlyTransactionService(BaseService):
             'month': schema.month,
             'amount': schema.amount,
             'tenant_id': local_tenant.id,
-            'tenant': local_tenant,
+            'service_id': 0,
         }
         return await self.save(model=tenants.MonthlyTransaction, **data)
 
