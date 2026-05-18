b6ff4c300bfb3a845e7f067600272d1a416d623f
xamidovscode <xamidovcoder@gmail.com>
2026-05-18 13:00:14 +0500

    crud

--- app/endpoints/users/routers.py
+++ app/endpoints/users/routers.py
@@ -5,31 +5,34 @@ from .services import user_service
 from ...models.choices import UserRoles
 from ...resources.permissions.dependencies import require_roles
 
-router = APIRouter(prefix="/users", tags=["users"])
+router = APIRouter(prefix="/users")
 
 
+
+
+# ---------- sellers crud apis ----------
 @router.post(
     '/create/',
     dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))],
-    response_model=schemas.SellerCreateResponseSchema
+    response_model=schemas.SellerCreateResponseSchema,
+    tags=["seller-crud"],
 )
 async def seller_create(schema: schemas.SellerCreateSchema, service: user_service):
     return await service.create_user(schema)
 
 
-@router.get('/list/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
+@router.get(
+    '/list/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))],
+    tags=["seller-crud"],
+)
 async def get_all_sellers(service: user_service):
     return await service.sellers_list()
-
-
-@router.get('/{seller_id}/', dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))])
-async def seller_detail(seller_id: int, service: user_service):
-    return await service.seller_detail(seller_id=seller_id)
-
 
 @router.patch(
     '/{seller_id}/',
     dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))],
+    tags=["seller-crud"],
 )
 async def seller_update(seller_id: int, schema: schemas.SellerUpdateSchema, service: user_service):
     return await service.update_seller(seller_id=seller_id, schema=schema)
@@ -38,7 +41,21 @@ async def seller_update(seller_id: int, schema: schemas.SellerUpdateSchema, serv
 @router.delete(
     '/{seller_id}/',
     dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))],
+    tags=["seller-crud"],
 )
 async def seller_delete(seller_id: int, service: user_service):
     return await service.delete_seller(seller_id=seller_id)
+
+
+
+
+# ---------- sellers detail apis ----------
+@router.get(
+    '/{seller_id}/',
+    dependencies=[Depends(require_roles(UserRoles.admin, UserRoles.super_admin))],
+    tags=["seller-detail"],
+)
+async def seller_detail(seller_id: int, service: user_service):
+    return await service.seller_detail(seller_id=seller_id)
+
 
